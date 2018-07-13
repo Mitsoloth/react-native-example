f6ec93004a4da3ab441c58b1278b91da083b46c0
Dimitrios Nomikos <d.nomikos@humanfactor.gr>
2018-07-13 13:00:29 +0300

    Initialized project

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>Hello World!</Text>
+      </View>
+    );
+  }
+}
+
---
+++ README.old.md
@@ -0,0 +1,2 @@
+# react-native-example
+Learning react
